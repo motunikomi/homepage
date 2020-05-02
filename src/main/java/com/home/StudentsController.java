8b8c441d2a0a57b53100aeffa99decd0913bafda
motunikomi <hasimoto910@gmail.com>
2020-05-02 22:07:17 +0900

    名前変更など

--- src/main/java/com/home/StudentsController.java
+++ src/main/java/com/home/StudentsController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class studentController {
+public class StudentsController {
 	@Controller
 	public class IndexController {
-		@RequestMapping(value = "/student")
+		@RequestMapping(value = "/students")
 		public String index(Model model) {
 			model.addAttribute("message", "トップページプルリク");
 			return "student";
