54511d1241bf44ad17fb2c19187050b030b8eba3
Ryoga home <1165133@cstu.nit.ac.jp>
2020-05-04 11:00:14 +0900

    changed part2

--- src/main/java/com/home/StudentsController.java
+++ src/main/java/com/home/StudentsController.java
@@ -3,15 +3,13 @@ package com.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Controller
 public class StudentsController {
-	@Controller
-	public class IndexController {
-		@RequestMapping(value = "/students")
-		public String index(Model model) {
-			model.addAttribute("message", "トップページプルリク");
-			return "student";
-		}
+
+	@RequestMapping(value = "/students")
+	public String index(Model model) {
+		model.addAttribute("message", "トップページプルリク");
+		return "students";
 	}
 
 }
